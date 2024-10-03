834619e75865a411ba4ac1ebada5c977f2dd2d75
steven3092 <stevenfauterrepro@gmail.com>
2024-10-03 17:25:25 +0200

    create interface for planet

---
+++ src/interfaces/planet.dto.tsx
@@ -0,0 +1,16 @@
+export interface PlanetDTO {
+  readonly climate: string;
+  readonly created: string;
+  readonly diameter: string;
+  readonly edited: string;
+  readonly films: string[];
+  readonly gravity: string;
+  readonly name: string;
+  readonly orbital_period: string;
+  readonly population: string;
+  readonly residents: string[];
+  readonly rotation_period: string;
+  readonly surface_water: string;
+  readonly terrain: string;
+  readonly url: string;
+}
