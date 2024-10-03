9320f5bb12e3916446fef7662d7a038ab98435ee
steven3092 <stevenfauterrepro@gmail.com>
2024-10-03 17:25:56 +0200

    optimization

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,5 +1,4 @@
 import { useSearchBar } from "./hooks/use-search-bar";
-// import "./search-bar.scss";
 
 export const SearchBar = ({
   handleOnCharacterSearch,
@@ -11,15 +10,13 @@ export const SearchBar = ({
   });
 
   return (
-    <>
-      <input
-        className="border border-gray-300 rounded-lg mx-4 my-4 py-4 px-4 pr-10 text-white focus:ring-blue-500"
-        type="text"
-        placeholder="Search characters"
-        value={searchQuery}
-        onChange={handleSearchChange}
-        name="search-characters"
-      />
-    </>
+    <input
+      className="border border-gray-300 rounded-lg mx-4 my-4 py-4 px-4 pr-10 text-white focus:ring-blue-500"
+      type="text"
+      placeholder="Search characters"
+      value={searchQuery}
+      onChange={handleSearchChange}
+      name="search-characters"
+    />
   );
 };
