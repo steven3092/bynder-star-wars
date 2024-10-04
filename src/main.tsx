bc5155edff6af57f0ffe1897e450798cc5096899
steven3092 <stevenfauterrepro@gmail.com>
2024-10-04 13:03:37 +0200

    add path to navigate to resident

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./components/error-page.tsx";
 import { Character } from "./components/character/character.tsx";
+import { Resident } from "./components/resident/resident.tsx";
 
 const queryClient = new QueryClient();
 
@@ -20,8 +21,12 @@ const router = createBrowserRouter([
     element: <Character />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "resident/:residentId/planet/:planetId",
+    element: <Resident />,
+    errorElement: <ErrorPage />,
+  },
 ]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
