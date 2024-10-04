91c240eedf3bf515b415d4123df39941c8046965
steven3092 <stevenfauterrepro@gmail.com>
2024-10-04 19:14:44 +0200

    remove bg black from search bar

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -11,7 +11,7 @@ export const SearchBar = ({
 
   return (
     <input
-      className="border border-gray-300 rounded-lg mx-4 my-4 py-4 px-4 pr-10 text-white focus:ring-blue-500"
+      className="border border-gray-300 rounded-lg mx-4 my-4 py-4 px-4 pr-10 text-black focus:ring-blue-500"
       type="text"
       placeholder="Search characters"
       value={searchQuery}
