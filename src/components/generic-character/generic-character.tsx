bcc630387552b25541450081b8b8313589c37f75
steven3092 <stevenfauterrepro@gmail.com>
2024-10-05 13:57:20 +0200

    improve css in detail page

--- src/components/generic-character/generic-character.tsx
+++ src/components/generic-character/generic-character.tsx
@@ -27,58 +27,75 @@ export const GenericCharacter = ({
       >
         <img src={backArrow} alt="back-arrow" />
       </button>
-      <div className="flex flex-col my-16 md:flex-row items-center p-6">
-        <ImageCharacter character={character} />
+      <div className="flex justify-center">
+        <div className="flex flex-col my-16 md:flex-row items-center p-6">
+          <div className="flex flex-col items-center">
+            <h1 className="text-xl font-bold mb-8">{character?.name}</h1>
+            <ImageCharacter character={character} />
+          </div>
 
-        <div className="flex-1">
-          <h2 className="text-xl font-bold">{character?.name}</h2>
-          <p>
-            <strong>Birth Year:</strong> {character?.birth_year}
-          </p>
-          <p>
-            <strong>Mass:</strong> {character?.mass}
-          </p>
-          <p>
-            <strong>Eye Color:</strong> {character?.eye_color}
-          </p>
-          <p>
-            <strong>Gender:</strong> {character?.gender}
-          </p>
-          <p>
-            <strong>Hair Color:</strong> {character?.hair_color}
-          </p>
-          <p>
-            <strong>Skin Color:</strong> {character?.skin_color}
-          </p>
-          <p>
-            <strong>Starships:</strong> {character?.starships.join(", ")}
-          </p>
-          <p>
-            <strong>Species:</strong> {character?.species}
-          </p>
-          <p>
-            <strong>Vehicles:</strong> {character?.vehicles.join(", ")}
-          </p>
+          <div className="flex flex-col sm:flex-row">
+            <div className="mx-8">
+              <h2 className="mt-4 mb-2 font-semibold text-lg">
+                Character Information :
+              </h2>
+              <p>
+                <strong>Birth Year:</strong> {character?.birth_year}
+              </p>
+              <p>
+                <strong>Mass:</strong> {character?.mass}
+              </p>
+              <p>
+                <strong>Eye Color:</strong> {character?.eye_color}
+              </p>
+              <p>
+                <strong>Gender:</strong> {character?.gender}
+              </p>
+              <p>
+                <strong>Hair Color:</strong> {character?.hair_color}
+              </p>
+              <p>
+                <strong>Skin Color:</strong> {character?.skin_color}
+              </p>
+              <p>
+                <strong>Starships:</strong> {character?.starships.join(", ")}
+              </p>
+              <p>
+                <strong>Species:</strong> {character?.species}
+              </p>
+              <p>
+                <strong>Vehicles:</strong> {character?.vehicles.join(", ")}
+              </p>
+            </div>
 
-          <h3 className="mt-4 font-semibold text-lg">Planet Information</h3>
-          <p>
-            <strong>Planet Name:</strong> {planet?.name}
-          </p>
-          <p>
-            <strong>Residents:</strong>{" "}
-            {planet?.residents.map((resident, index) => (
-              <li key={index}>
-                <button
-                  type="button"
-                  onClick={() =>
-                    handleOnClickGenericCharacter(resident, character.homeworld)
-                  }
-                >
-                  Resident {index + 1}
-                </button>
-              </li>
-            ))}
-          </p>
+            <div className="mx-8">
+              <h2 className="mt-4 mb-2 font-semibold text-lg">
+                Planet Information :
+              </h2>
+              <p>
+                <strong>Planet Name:</strong> {planet?.name}
+              </p>
+              <p>
+                <strong>Residents:</strong>{" "}
+                {planet?.residents.map((resident, index) => (
+                  <li key={index}>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        handleOnClickGenericCharacter(
+                          resident,
+                          character.homeworld
+                        )
+                      }
+                      className="text-white p-1 px-4 my-1 bg-gray-400 rounded-lg shadow-md hover:bg-gray-700 transition-colors"
+                    >
+                      Resident {index + 1}
+                    </button>
+                  </li>
+                ))}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import ErrorPage from "./components/error-page.tsx";
 import { Character } from "./components/character/character.tsx";
 import { Resident } from "./components/resident/resident.tsx";
+import ErrorPage from "./components/error-page.tsx";
+import App from "./App.tsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
